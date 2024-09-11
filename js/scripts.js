27392831974d95f8f3f9a89ae15fdea50e39d48a
logicless-bt <benpturner01@gmail.com>
2024-09-10 21:27:18 -0400

    introduced IIFE and replaced forEach loop
    -wrapped pokemonList in an IIFE
    -replaced the previous forEach loop with one that can access the newly wrapped pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,37 @@
-let pokemonList = [
-{name: 'Sigilyph', height: 4, types: ['psychic', 'flying']},
-{name: 'Falinks', height: 9, types: ['fighting']},
-{name: 'Volcarona', height: 5, types: ['bug', 'fire']}
-];
+let pokemonRepository = (function () {
+	let pokemonList = [
+		{
+			name: 'Sigilyph', 
+			height: 4, 
+			types: ['psychic', 'flying']
+		},
+		{
+			name: 'Falinks', 
+			height: 9, 
+			types: ['fighting']
+		},
+		{
+			name: 'Volcarona', 
+			height: 5, 
+			types: ['bug', 'fire']
+		}
+	]
 
-function readOut(element) {
+	function add (pokemon) {
+		pokemonList.push(pokemon);
+	}
+
+	function getAll () {
+		return pokemonList;
+	}
+
+	return {
+		add: add,
+		getAll: getAll
+	};
+})();
+
+/*function readOut(element) {
 	if (element.height < 5) {
 		document.write(`${element.name} \(height: ${element.height}\) -- that's a small one! `);
 		document.write('<br\>');
@@ -12,6 +39,14 @@ function readOut(element) {
 		document.write(`${element.name} \(height: ${element.height}\) `);
 		document.write('<br\>');
 	}
-}
+}*/
 
-pokemonList.forEach(readOut);
+pokemonRepository.getAll().forEach(function(element) {
+if (element.height < 5) {
+		document.write(`${element.name} \(height: ${element.height}\) -- that's a small one! `);
+		document.write('<br\>');
+	} else {
+		document.write(`${element.name} \(height: ${element.height}\) `);
+		document.write('<br\>');
+	}
+});
