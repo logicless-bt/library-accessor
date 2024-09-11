542a120e22e46c4ff505398f53190d54152e4faf
logicless-bt <benpturner01@gmail.com>
2024-09-11 14:26:07 -0400

    styled and created AddListItem
    -styled buttons and media queries
    -changed the forEach loop into a function wrapped within the pokemonRepository IIFE
    -documented

--- js/scripts.js
+++ js/scripts.js
@@ -25,9 +25,37 @@ let pokemonRepository = (function () {
 		return pokemonList;
 	}
 
+	function addListItem(pokemon) {
+		//setting up
+		let pokemonCurrent = document.querySelector('.pokemon-list');
+		let listItem = document.createElement('li');
+		let button = document.createElement('button');
+		button.addEventListener('click', showDetails(pokemon));
+		// for some reason the above code immediately displays the information
+
+		/*button.addEventListener('click', function(pokemon) {
+			console.log(pokemon);
+		})*/
+		//the above code works correctly, but does not use the predefined function
+
+		//changing content and class of buttons, event listener
+		button.innerText = pokemon.name;
+		button.classList.add('pokemonButton');
+
+		//placing button on list
+		listItem.appendChild(button);
+		pokemonCurrent.appendChild(listItem);
+	}
+
+	function showDetails(pokemon) {
+		console.log(pokemon);
+	}
+
 	return {
 		add: add,
-		getAll: getAll
+		getAll: getAll,
+		addListItem: addListItem,
+		showDetails: showDetails
 	};
 })();
 
@@ -41,12 +69,6 @@ let pokemonRepository = (function () {
 	}
 }*/
 
-pokemonRepository.getAll().forEach(function(element) {
-if (element.height < 5) {
-		document.write(`${element.name} \(height: ${element.height}\) -- that's a small one! `);
-		document.write('<br\>');
-	} else {
-		document.write(`${element.name} \(height: ${element.height}\) `);
-		document.write('<br\>');
-	}
+pokemonRepository.getAll().forEach(function(pokemon) {
+	pokemonRepository.addListItem(pokemon);
 });
